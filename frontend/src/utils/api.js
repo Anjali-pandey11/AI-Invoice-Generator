3fe2debb6b5715e2ff1a863c2c059dabc3641ebc
Anjali-pandey11 <pandeyanjali7906@gmail.com>
2026-05-18 00:20:47 +0530

    register

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+console.log('BASE URL:', import.meta.env.VITE_BASE_URL);
+
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default api;
